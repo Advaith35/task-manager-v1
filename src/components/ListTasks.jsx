b9573875f750a6788dc7d3c10caaf2aaff28a6bf
Advaith35 <71253474+Advaith35@users.noreply.github.com>
2024-03-08 14:58:09 +0530

    Update ListTasks.jsx

--- src/components/ListTasks.jsx
+++ src/components/ListTasks.jsx
@@ -1,33 +1,110 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+
 
 const ListTasks = ({ tasks, setTasks }) => {
+  const [todos, setTodos] = useState([]);
+  const [inProgress, setInProgress] = useState([]);
+  const [closed, setClosed] = useState([]);
 
-    const [todos, setTodos] = useState([]);
-    const [inProgress, setInProgress] = useState([]);
-    const [closed, setClosed] = useState([]);
+  useEffect(() => {
+    const fTodos = tasks.filter((task) => task.status === "todo" && task.name.trim() !== "");
+    const fInProgress = tasks.filter((task) => task.status === "inprogress" && task.name.trim() !== "");
+    const fClosed = tasks.filter((task) => task.status === "closed" && task.name.trim() !== "");
 
-    useEffect(() => {
+    setTodos(fTodos);
+    setInProgress(fInProgress);
+    setClosed(fClosed);
+}, [tasks]);
 
-        const fTodos = tasks.filter((task) => task.status === "todo");
-        const fInProgress = tasks.filter((task) => task.status === "inprogress");
-        const fClosed = tasks.filter((task) => task.status === "closed");
+  const statuses = ["todo", "inprogress", "closed"];
 
-        setTodos(fTodos);
-        setInProgress(fInProgress);
-        setClosed(fClosed);
-
-    }, [tasks]);
-
-    const statuses = ["todo", "inprogress", "closed"]
-
-    return 
-     <div>
-    {statuses.map((status , index) => <Section key = {index} status  = {status} /> )}
-    </div>;
-};  
+  return (
+    <div className="flex gap-16">
+      {statuses.map((status, index) => (
+        <Section
+          key={index}
+          status={status}
+          tasks={tasks}
+          setTasks={setTasks}
+          todos={todos}
+          inProgress={inProgress}
+          closed={closed}
+        />
+      ))}
+    </div>
+  );
+};
 export default ListTasks;
 
+const Section = ({ status, tasks, setTasks, todos, inProgress, closed }) => {
+  let text = "Todo";
+  let bg = "bg-slate-500";
+  let tasksToMap = todos;
 
-const Section = () => {
-    return <>Header List</>;
+  if (status === "inprogress") {
+    text = "In Progress";
+    bg = "bg-purple-500";
+    tasksToMap = inProgress;
+  }
+  if (status === "closed") {
+    text = "Closed";
+    bg = "bg-green-500";
+    tasksToMap = closed;
+  }
+
+  return (
+    <div className={"w-64 "}>
+      <Header text={text} bg={bg} count={tasksToMap.length} />
+      {tasksToMap.length > 0 &&
+        tasksToMap.map((task) => (
+          <Task key={task.id} task={task} tasks={tasks} setTasks={setTasks} />
+        ))}
+    </div>
+  );
+};
+
+const Header = ({ text, bg, count }) => {
+  return (
+    <div
+      className={`${bg} flex items-center h-12 pl-4 rounded-md uppercase text-sm text-white`}
+    >
+      {text}
+      <div className="ml-2 bg-white w-5 h-5 text-black rounded-full flex items-center justify-center">
+        {count}
+      </div>
+    </div>
+  );
+};
+
+const Task = ({ task, tasks, setTasks }) => {
+    const handelRemove = (id) => {
+        console.log(id);
+        const fTasks = tasks.filter((t) => t.id !== id);
+
+        setTasks(fTasks);
+
+        toast("Task removed successfully",{icon : "☠️"});
+    };
+  return (
+    <div className={`relative p-4 mt-8 shadow-md rounded-md cursor-grab`}>
+      <p>{task.name}</p>
+      <button className="absolute bottom-1 right-1 text-slate-400" onClick={() => handelRemove(task.id)}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+          />
+        </svg>
+      </button>
+    </div>
+  );
 };
